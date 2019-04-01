526368ae3128f0244a631ee43fd6de6158a0991a
cutajarj2 <james.cutajar@blip.pt>
2019-04-01 10:11:29 +0100

    Adding docs to lesson 6

--- src/main/java/com/packt/jamescutajar/lesson6/LevenshteinDistance.java
+++ src/main/java/com/packt/jamescutajar/lesson6/LevenshteinDistance.java
@@ -1,6 +1,10 @@
 package com.packt.jamescutajar.lesson6;
 
 public class LevenshteinDistance {
+    /**
+     * Exercise for Lesson 6.
+     * Implement the Levenshtein distnace algorithm in this method as explained in the reading material
+     */
     public int stringDistance(String strA, String strB) {
         return -1;
     }
