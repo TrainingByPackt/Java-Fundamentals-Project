ce65aac599a8f299c783077f236d47125a9aa7a3
cutajarj2 <james.cutajar@blip.pt>
2019-04-01 10:08:14 +0100

    Adding lesson 6 and 7 solutions

--- src/main/java/com/packt/jamescutajar/lesson6/LevenshteinDistance.java
+++ src/main/java/com/packt/jamescutajar/lesson6/LevenshteinDistance.java
@@ -2,6 +2,23 @@ package com.packt.jamescutajar.lesson6;
 
 public class LevenshteinDistance {
     public int stringDistance(String strA, String strB) {
-        return -1;
+        int[][] dist = new int[strA.length() + 1][strB.length() + 1];
+        for (int i = 0; i <= strA.length(); i++) dist[i][0] = i;
+        for (int i = 0; i <= strB.length(); i++) dist[0][i] = i;
+
+        for (int x = 0; x < strA.length(); x++) {
+            for (int y = 0; y < strB.length(); y++) {
+                if (strA.charAt(x) == strB.charAt(y))
+                    dist[x + 1][y + 1] = dist[x][y];
+                else {
+                    int a = dist[x][y], b = dist[x + 1][y],
+                            c = dist[x][y + 1];
+                    dist[x + 1][y + 1] =
+                            Math.min(a, Math.min(b, c)) + 1;
+                }
+            }
+        }
+
+        return dist[strA.length()][strB.length()];
     }
 }
--- src/main/java/com/packt/jamescutajar/lesson7/InOrderSuccAndPre.java
+++ src/main/java/com/packt/jamescutajar/lesson7/InOrderSuccAndPre.java
@@ -1,6 +1,9 @@
 package com.packt.jamescutajar.lesson7;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Exercise for Lesson 7.
@@ -8,15 +11,25 @@ import java.util.Optional;
  */
 public class InOrderSuccAndPre {
 
+    private SortedSet<Integer> sortedSet = new TreeSet<>();
+
     public InOrderSuccAndPre(Integer[] numbers) {
+        sortedSet.addAll(Arrays.asList(numbers));
     }
 
     public Optional<Integer> successor(Integer x) {
-        return Optional.of(-1);
+        SortedSet<Integer> tailSet = sortedSet.tailSet(x + 1);
+        if (tailSet.isEmpty())
+            return Optional.empty();
+        else
+            return Optional.of(tailSet.first());
     }
 
     public Optional<Integer> predecessor(int x) {
-        return Optional.of(-1);
+        SortedSet<Integer> headSet = sortedSet.headSet(x);
+        if (headSet.isEmpty())
+            return Optional.empty();
+        else
+            return Optional.of(headSet.last());
     }
-
 }
