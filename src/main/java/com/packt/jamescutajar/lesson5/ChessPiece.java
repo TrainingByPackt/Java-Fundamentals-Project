c31289fc7e8dad988ee43a17e9229adf371fb77e
cutajarj2 <james.cutajar@blip.pt>
2019-04-02 17:04:40 +0100

    lesson 5 comments

--- src/main/java/com/packt/jamescutajar/lesson5/ChessPiece.java
+++ src/main/java/com/packt/jamescutajar/lesson5/ChessPiece.java
@@ -4,6 +4,11 @@ import com.packt.jamescutajar.lesson5.moves.MoveValidator;
 
 import java.util.List;
 
+/**
+ * Top level abstract class for lesson 5.
+ * Extend this class as explained in the reading material.
+ * In addition implement the moveTo method
+ */
 public abstract class ChessPiece {
     private String currentPosition;
 
