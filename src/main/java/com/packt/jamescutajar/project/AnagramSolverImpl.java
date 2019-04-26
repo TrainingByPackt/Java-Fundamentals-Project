2753dfe281e2aa945baaf969866c6ec51a9182d5
cutajarj2 <james.cutajar@blip.pt>
2019-04-26 17:50:29 +0100

    adding final project solution

---
+++ src/main/java/com/packt/jamescutajar/project/AnagramSolverImpl.java
@@ -0,0 +1,73 @@
+package com.packt.jamescutajar.project;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.*;
+
+public class AnagramSolverImpl implements AnagramSolver {
+    private Set<String> allWords = new HashSet<>();
+    private Map<String, List<String>> anagramToWords = new HashMap<>();
+    private ArrayList<String> wordList = new ArrayList<>();
+    private Random rand = new Random();
+
+    @Override
+    public void loadWords(File textFile) throws ParseException, IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(textFile));
+        String word = fileReader.readLine();
+        int lineNumber = 1;
+        while (word != null) {
+            if (!word.matches("[a-z]+"))
+                throw new ParseException("input textfile should only contain lower case " +
+                        "characters 'a' to 'z' at line number " + lineNumber, lineNumber);
+            allWords.add(word);
+            wordList.add(word);
+            char[] characters = word.toCharArray();
+            Arrays.sort(characters);
+            String sortedAnagram = new String(characters);
+            anagramToWords.putIfAbsent(sortedAnagram, new ArrayList<>());
+            anagramToWords.putIfAbsent(sortedAnagram, new ArrayList<>());
+            anagramToWords.get(sortedAnagram).add(word);
+            word = fileReader.readLine();
+            lineNumber++;
+        }
+    }
+
+    @Override
+    public String pickRandomAnagram() {
+        int randPick = rand.nextInt(allWords.size());
+        StringBuilder randWord = new StringBuilder(wordList.get(randPick));
+        for (int i = randWord.length() - 1; i > 0; i--) {
+            int randCharIdx = rand.nextInt(i + 1);
+            char charPick = randWord.charAt(randCharIdx);
+            randWord.setCharAt(randCharIdx, randWord.charAt(i));
+            randWord.setCharAt(i, charPick);
+        }
+        return randWord.toString();
+    }
+
+    @Override
+    public boolean validateAttempt(String anagram, String attempt) {
+        //First check if the attempt string has the same character count as the anagram
+        int[] charCount = new int[256];
+        for (int i = 0; i < anagram.length(); i++)
+            charCount[anagram.charAt(i)]++;
+        for (int i = 0; i < attempt.length(); i++)
+            charCount[attempt.charAt(i)]--;
+        for (int i1 : charCount) if (i1 != 0) return false;
+        //Then check if the attempt string exist in the dictionary
+        return allWords.contains(attempt);
+    }
+
+    public List<String> findSolutions(String anagram) {
+        char[] characters = anagram.toCharArray();
+        Arrays.sort(characters);
+        String sortedAnagram = new String(characters);
+        return anagramToWords.get(sortedAnagram);
+    }
+}
+
+
+
--- src/main/java/com/packt/jamescutajar/project/AnagramSolverLauncher.java
+++ src/main/java/com/packt/jamescutajar/project/AnagramSolverLauncher.java
@@ -7,5 +7,32 @@ import java.util.Scanner;
 
 public class AnagramSolverLauncher {
     public static void main(String[] args) throws IOException, ParseException {
+        AnagramSolverImpl solver = new AnagramSolverImpl();
+        solver.loadWords(new File("src/main/resources/fiveLetters.txt"));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("*** Welcome to Anagram Solver! ***");
+        System.out.println("Can you solve the following anagram to make an english word?");
+        String anagram = solver.pickRandomAnagram();
+        int count = 1;
+        boolean successful = false;
+        String attempt = null;
+        while (count <= 5 && !successful) {
+            System.out.println("The anagram is: '" + anagram +
+                    "', attempt " + count + " of 5");
+            attempt = scanner.nextLine();
+            successful = solver.validateAttempt(anagram, attempt);
+            if (!successful)
+                System.out.println("The word '" + attempt + "' is not in my dictionary!");
+            count++;
+        }
+        if (successful) {
+            System.out.println("The word '" + attempt + "' is a correct solution!");
+            System.out.println("In my dictionary I have the following valid word(s): ");
+            System.out.println(solver.findSolutions(anagram));
+        } else {
+            System.out.println("No more attempts available. The correct solution(s) are: ");
+            System.out.println(solver.findSolutions(anagram));
+        }
     }
 }
