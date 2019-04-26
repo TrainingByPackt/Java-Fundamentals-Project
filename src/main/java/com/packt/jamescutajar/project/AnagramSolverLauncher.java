5062712fd733832e75431acc4b3936a8a47bb29b
cutajarj2 <james.cutajar@blip.pt>
2019-04-26 17:54:35 +0100

    adding final project solution

--- src/main/java/com/packt/jamescutajar/project/AnagramSolverLauncher.java
+++ src/main/java/com/packt/jamescutajar/project/AnagramSolverLauncher.java
@@ -6,6 +6,22 @@ import java.text.ParseException;
 import java.util.Scanner;
 
 public class AnagramSolverLauncher {
+
+    /**
+     * Implement a main method that behaves in the following manner using the
+     * implementation for the AnagramSolver
+     *
+     * *** Welcome to Anagram Solver! ***
+     * Can you solve the following anagram to make an english word?
+     * The anagram is: 'eapgr', attempt 1 of 5
+     * gepra
+     * The word 'gepra' is not in my dictionary!
+     * The anagram is: 'eapgr', attempt 2 of 5
+     * pager
+     * The word 'pager' is a correct solution!
+     * In my dictionary I have the following valid word(s):
+     * [gaper, grape, pager, parge]
+     */
     public static void main(String[] args) throws IOException, ParseException {
     }
 }
