4e76c03da3deea2916cf8562a2231ec04b973d31
cutajarj2 <james.cutajar@blip.pt>
2019-04-30 11:50:49 +0100

    Adding solution for lesson 1

--- src/main/java/com/packt/jamescutajar/lesson1/RecordCapture.java
+++ src/main/java/com/packt/jamescutajar/lesson1/RecordCapture.java
@@ -9,9 +9,27 @@ import java.util.Scanner;
 public class RecordCapture {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your name");
-        String name = scanner.next();
-        System.out.println("Hello " + name + "!");
-        System.out.println("Your name is " + name.length() + " characters long");
+        System.out.println("*** Welcome to the Vinyl Record Store's Capture Screen ***");
+        System.out.println();
+        System.out.println("What is the album name?");
+        String albumName = scanner.nextLine();
+        System.out.println("What is the name of the Artist?");
+        String artistName = scanner.nextLine();
+        System.out.println("How many tracks does the album have?");
+        int tracks = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is the name of the label?");
+        String labelName = scanner.nextLine();
+        System.out.println("Which year was this album released?");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+        System.out.println("Album card");
+        System.out.println("----------");
+        System.out.println(String.format("Album name: \"%s\"", albumName));
+        System.out.println(String.format("Artist: \"%s\"", artistName));
+        System.out.println(String.format("Total Tracks: \"%s\"", tracks));
+        System.out.println(String.format("Label: \"%s\"", labelName));
+        System.out.println(String.format("Year Released: \"%s\"", year));
     }
 }
